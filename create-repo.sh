4c21ce0e784a25467e0f99691945e328f1cdefde
Sun <sunping1415926@gmail.com>
2020-03-15 16:47:42 +0800

    add repo creation script

---
+++ create-repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <repository-name>"
+    exit 1
+fi
+
+REPO_NAME=$1
+REPO_PATH="/var/git/${REPO_NAME}.git"
+
+if [ -d "$REPO_PATH" ]; then
+    echo "Repository $REPO_NAME already exists"
+    exit 1
+fi
+
+sudo -u git mkdir -p "$REPO_PATH"
+cd "$REPO_PATH"
+sudo -u git git init --bare
+
+echo "Repository $REPO_NAME created at $REPO_PATH"
