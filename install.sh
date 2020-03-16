ee90f38779b6c2b7350b20574d1f7f91d979d2ee
Sun <sunping1415926@gmail.com>
2020-03-16 10:55:19 +0800

    update installer with nginx setup

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git nginx
+apt-get install -y git nginx fcgiwrap
 
 adduser --system --group --shell /bin/bash git
 mkdir -p /var/git
@@ -9,3 +9,11 @@ chown git:git /var/git
 
 su - git -c "git config --global user.name 'Git Server'"
 su - git -c "git config --global user.email 'git@server.local'"
+
+cp nginx-git.conf /etc/nginx/sites-available/git
+ln -sf /etc/nginx/sites-available/git /etc/nginx/sites-enabled/
+rm -f /etc/nginx/sites-enabled/default
+
+systemctl enable fcgiwrap
+systemctl start fcgiwrap
+systemctl reload nginx
